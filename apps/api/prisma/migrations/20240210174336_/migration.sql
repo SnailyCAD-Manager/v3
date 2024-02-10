356f14ab1675bb1aa50d9df25f74e6cd8275f66f
Whitigol <whitetigerwolfthe1st@gmail.com>
2024-02-10 12:57:00 -0500

    🎉 feat: prep password reset

---
+++ apps/api/prisma/migrations/20240210174336_/migration.sql
@@ -0,0 +1,14 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "passwordResetAtNextLogin" BOOLEAN NOT NULL DEFAULT false,
+    "role" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("id", "password", "role", "username") SELECT "id", "password", "role", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
